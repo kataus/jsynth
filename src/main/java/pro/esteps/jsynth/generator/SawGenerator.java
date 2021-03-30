adb9815d135a0843c2919590d7b90c66547b72da
DmitryKirillov <esteps.kirillov@gmail.com>
2021-03-30 10:45:02 +0300

    Develop click-free Generator with infinite periods

--- src/main/java/pro/esteps/jsynth/generator/SawGenerator.java
+++ src/main/java/pro/esteps/jsynth/generator/SawGenerator.java
@@ -2,25 +2,55 @@ package pro.esteps.jsynth.generator;
 
 public class SawGenerator implements Generator {
 
+    private float frequency;
+
+    /**
+     * Cursor position within current period
+     */
+    private int currentPeriodCursorIndex;
+
+    private byte[] period = new byte[0];
+
+    private void regeneratePeriod() {
+
+        int currentPeriodSize = period.length;
+        int newPeriodSize = (int) (44100 / frequency);
+        period = new byte[newPeriodSize];
+
+        int index = 0;
+        byte sample;
+        float divider = newPeriodSize / 256f;
+        for (int i = newPeriodSize; i > 0; i--) {
+            sample = (byte) (i / divider - 128);
+            period[index++] = sample;
+        }
+
+        if (currentPeriodCursorIndex > 0) {
+            if (currentPeriodCursorIndex == currentPeriodSize - 1) {
+                currentPeriodCursorIndex = newPeriodSize - 1;
+            } else {
+                currentPeriodCursorIndex = currentPeriodCursorIndex * newPeriodSize / currentPeriodSize;
+            }
+        }
+
+    }
+
     @Override
     public byte[] generateChunk(float frequency) {
+
+        if (this.frequency != frequency) {
+            this.frequency = frequency;
+            this.regeneratePeriod();
+        }
+
         byte[] chunk = new byte[2048];
-        int segmentSize = (int) (44100 / frequency);
-        int fromIndex = 0;
-        byte sample;
-        do {
-            float divider = segmentSize / 256f;
-            for (int i = segmentSize; i > 0; i--) {
-                sample = (byte) (i / divider - 128);
-                chunk[fromIndex++] = sample;
-                if (fromIndex == chunk.length) {
-                    break;
-                }
+        for (int i = 0; i < chunk.length; i++) {
+            chunk[i] = period[currentPeriodCursorIndex++];
+            if (currentPeriodCursorIndex >= period.length) {
+                currentPeriodCursorIndex = 0;
             }
-            if (fromIndex == chunk.length) {
-                break;
-            }
-        } while (true);
+        }
+
         return chunk;
     }
 
